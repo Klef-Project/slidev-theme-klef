5086a43e9fc98d1b5b7c7899588a3b56f87597c6
haru-0205 <rm23081b@osaka-pct.ac.jp>
2024-05-30 00:48:30 +0900

    Add Bottom Line

---
+++ global-top.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer class="absolute bottom-0 left-0 right-0 p-0 p-l-2 p-r-2 bg-sky-200 flex-container">
+        <p class="text-s p-0 m-0">{{ $slidev.configs.title }}</p>
+        <div class="flex-c2 p-0 p-l-2 p-r-2">
+            <carbon:logo-github />
+            <p class="text-s p-0 m-0">{{ $slidev.configs.file }}</p>
+            <p class="text-s p-0 m-0 p-l-4"># {{ $slidev.nav.currentPage }}</p>
+        </div>
+    </footer>
+</template>
+
+<style>
+    .flex-container{
+        display: flex;
+        flex-wrap: nowrap;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .flex-c2{
+        display: flex;
+        flex-wrap: nowrap;
+        justify-content: end;
+        align-items: center;
+    }
+</style>
